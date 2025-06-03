03695c4326b87b588a818040a7df4e9ba3032ed5
Анастасия <anastasiav@Anastasias-MacBook-Air.local>
2025-06-03 16:55:03 +0300

    main: added location management

--- BrightWeatherApp/Controllers/WeatherViewController.swift
+++ BrightWeatherApp/Controllers/WeatherViewController.swift
@@ -13,17 +13,28 @@ class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        
-        view.addSubview(primaryView)
-        NSLayoutConstraint.activate([
-            primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            primaryView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            primaryView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
+        setUpView()
+        getLocation()
     }
 
-
-}
-
+    private func getLocation() {
+        LocationManager.shared.getCurrentLocation { location in
+            print(String(describing: location))
+            
+            WeatherManager.shared.getWeather(for: location)
+        }
+    }
+            
+    private func setUpView() {
+                view.backgroundColor = .systemBackground
+                
+                view.addSubview(primaryView)
+                NSLayoutConstraint.activate([
+                    primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                    primaryView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+                    primaryView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+                    primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+                ])
+            }
+        }
+    
--- BrightWeatherApp/Managers/LocationManager.swift
+++ BrightWeatherApp/Managers/LocationManager.swift
@@ -7,8 +7,40 @@
 
 import CoreLocation
 import Foundation
+import UIKit
 
-final class LocationManager {
-    static let shared = LocationManager()
-    
-}
+    final class LocationManager: NSObject, CLLocationManagerDelegate {
+        
+        private let manager = CLLocationManager()
+        
+        static let shared = LocationManager()
+        
+        private var locationFetchCompletion: ((CLLocation) -> Void)?
+        
+        private var location: CLLocation? {
+            didSet {
+                guard let location else {
+                    return
+                }
+                locationFetchCompletion?(location)
+            }
+        }
+        
+        public func getCurrentLocation(completion: @escaping (CLLocation) -> Void) {
+            
+            self.locationFetchCompletion = completion
+            
+            manager.requestWhenInUseAuthorization()
+            manager.delegate = self
+            manager.startUpdatingLocation()
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let location = locations.first else {
+                return
+            }
+            self.location = location
+            manager.startUpdatingLocation()
+        }
+    }
+
